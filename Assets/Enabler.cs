987cda58d49c53222c5809ce826f4793e2968043
MaesterLazer <daniel.l.lazar@gmail.com>
2018-04-05 02:04:10 -0700

    +Playmode activator, Physics heavily tweaked, +driving range, wedge angle now 45deg

---
+++ Assets/Enabler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enabler : MonoBehaviour {
+
+    // The list of things we'll enable on start
+    public List<GameObject> enablees;
+
+    // the number of enablees
+    int enableeCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        enableeCount = enablees.Count; // How many enablees are in the list?
+        for (int i = 0; i < enablees.Count; i++)
+        {
+            enablees[i].gameObject.SetActive(true); // Deactivate all enablees in case we forgot to elsewhere
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
