f0b48be4db8edf52a8d9132c9820b035864f4a04
MaesterLazer <daniel.l.lazar@gmail.com>
2018-04-08 16:40:47 -0700

    cleanup, tile origin fix, level 1 progress, placeholder hole script

---
+++ Assets/_Scripts/UIMaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMaster : MonoBehaviour {
+
+    public GameObject speedometer;
+    public GameObject clubIcon;
+    public GameObject strokeCount;
+    public GameObject courseName;
+    public GameObject holeDistance;
+
+
+}
