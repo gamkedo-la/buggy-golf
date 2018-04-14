21208d221e4d949b90cd9f0f627bf5ebbe5c7f94
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2018-04-15 03:41:47 +0800

    Removed unused namespaces.

--- Assets/_Scripts/Leaderboards.cs
+++ Assets/_Scripts/Leaderboards.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.SceneManagement;
