288b78a49efed84b9e89532e39df5d8d4c1ed292
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2018-04-15 01:43:59 +0800

    Added Leaderboards script. ScoreManager requires Leaderboards component. Leaderboards automatically sort names according to scores. Leaderboards methods currently: AddScore(), DeleteLeaderboards(), GetNameByRank(), GetScoreByRank(), GetScoreByName(), DeleteLeaderboards(). Possibly buggy. :)

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Leaderboards))]
 public class ScoreManager : MonoBehaviour {
+    Leaderboards leaderboards;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        //
+        // BEGIN Leaderboards Test
+        //
+        Debug.Log("Testing Leaderboards!!!");
+
+        leaderboards = GetComponent<Leaderboards>();
+
+        // Delete leaderboards file.
+        leaderboards.DeleteLeaderboards();
+
+        // Add scores to leaderboards.
+        leaderboards.AddScore("Might Morphin Rocket Ranger", 10);
+        leaderboards.AddScore("Cat Dog Octopus", 99);
+        leaderboards.AddScore("See Sharp See Blunt", 99);
+        leaderboards.AddScore("Biker Mice From Mars", 15);
+        leaderboards.AddScore("Load Runner", 5123);
+
+        Debug.Log(leaderboards.GetScoreByName("Biker Mice From Mars"));
+        Debug.Log(leaderboards.GetScoreByName("Cat Dog Octopus"));
+        Debug.Log(leaderboards.GetScoreByName("Does Not Exist!")); // doesn't exist
+
+        Debug.Log(leaderboards.GetNameByRank(1) + " has a score of " + leaderboards.GetScoreByRank(1));
+        Debug.Log(leaderboards.GetNameByRank(2) + " has a score of " + leaderboards.GetScoreByRank(2));
+        Debug.Log(leaderboards.GetNameByRank(3) + " has a score of " + leaderboards.GetScoreByRank(3));
+        Debug.Log(leaderboards.GetNameByRank(4) + " has a score of " + leaderboards.GetScoreByRank(4));
+        Debug.Log(leaderboards.GetNameByRank(5) + " has a score of " + leaderboards.GetScoreByRank(5));
+
+        // the following don't exist
+        Debug.Log(leaderboards.GetNameByRank(6) + " has a score of " + leaderboards.GetScoreByRank(6));
+        Debug.Log(leaderboards.GetNameByRank(7) + " has a score of " + leaderboards.GetScoreByRank(7));
+        Debug.Log(leaderboards.GetNameByRank(8) + " has a score of " + leaderboards.GetScoreByRank(8));
+        Debug.Log(leaderboards.GetNameByRank(9) + " has a score of " + leaderboards.GetScoreByRank(9));
+        Debug.Log(leaderboards.GetNameByRank(10) + " has a score of " + leaderboards.GetScoreByRank(10));
+
+        //
+        // END Leaderboards Test
+        //
+    }
 }
---
+++ Assets/_Scripts/Leaderboards.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Leaderboards : MonoBehaviour {
+    [SerializeField] string leaderboardsFileName = "leaderboards";
+
+    string currentSceneName="";
+
+    Dictionary<string, int> scores = new Dictionary<string, int>();
+    Dictionary<string, Dictionary<string, int>> courseScores = new Dictionary<string, Dictionary<string, int>>();
+
+    void Awake() {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        LoadAllScores();                      
+    }
+
+    /// <summary>
+    /// Adds a score to the leaderboard of the current scene and saves it to the leaderboards file.
+    /// </summary>
+    /// <param name="playerName">The player's name.</param>
+    /// <param name="playerScore">The player's score</param>
+    public void AddScore(string playerName, int playerScore) {
+        LoadAllScores();
+
+        if (scores.ContainsKey(playerName)) {
+            if (playerScore > scores[playerName]) {
+                scores[playerName] = playerScore;
+            }
+        }
+        else {
+            scores.Add(playerName, playerScore);
+        }
+
+        scores = scores.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+        if (courseScores.ContainsKey(currentSceneName)) {
+            courseScores[currentSceneName] = scores;
+        }
+        else {
+            courseScores.Add(currentSceneName, scores);
+        }
+
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream fileStream = File.Create(Application.persistentDataPath + "/" + leaderboardsFileName);
+
+        binaryFormatter.Serialize(fileStream, courseScores);
+        fileStream.Close();
+    }
+    
+    /// <summary>
+    /// Loads and deserializes all scores across all leaderboards.
+    /// </summary>
+    void LoadAllScores() {
+        if (File.Exists(Application.persistentDataPath + "/" + leaderboardsFileName)) {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = File.Open(Application.persistentDataPath + "/" + leaderboardsFileName, FileMode.Open);
+
+            Dictionary<string, Dictionary<string, int>> courseScores = 
+                    (Dictionary<string, Dictionary<string, int>>)binaryFormatter.Deserialize(fileStream);
+
+            fileStream.Close();
+
+            this.courseScores = courseScores;
+        }
+    }    
+
+    /// <summary>
+    /// Deletes the leaderboards file. This clears all leaderboards across all scenes.
+    /// </summary>
+    public void DeleteLeaderboards() {
+        File.Delete(Application.persistentDataPath + "/" + leaderboardsFileName);
+    }
+
+    /// <summary>
+    /// Get player's score by the player's name.
+    /// </summary>
+    /// <param name="playerName">The player's name.</param>
+    /// <returns></returns>
+    public int GetScoreByName(string playerName) {
+        Dictionary<string, int> scrs = courseScores[currentSceneName];
+
+        if (scrs.ContainsKey(playerName)) {
+            return scrs[playerName];
+        }
+        else {
+            return -1;
+        }
+    }
+
+    /// <summary>
+    /// Get player's score by the player's rank.
+    /// </summary>
+    /// <param name="rank">The player's rank.</param>
+    /// <returns></returns>
+    public int GetScoreByRank(int rank) {
+        if (scores.Count > 0) rank -= 1;
+
+        if (rank < scores.Count) {
+            return scores.ElementAt(rank).Value;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    /// <summary>
+    /// Get player's name  by the player's rank.
+    /// </summary>
+    /// <param name="rank">The player's rank.</param>
+    /// <returns></returns>
+    public string GetNameByRank(int rank) {
+        if (scores.Count > 0) rank -= 1;
+
+        if (rank < scores.Count) {
+            return scores.ElementAt(rank).Key;
+        }
+        else {
+            return "THIS PLAYER DOES NOT EXIST!!!";
+        }
+    }
+}
