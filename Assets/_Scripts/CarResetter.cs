6fd3aa4e55c54159905ce64127abc90a7683a551
MaesterLazer <daniel.l.lazar@gmail.com>
2018-04-14 16:46:36 -0700

    postshot car placement UI (in progress)

--- Assets/_Scripts/CarResetter.cs
+++ Assets/_Scripts/CarResetter.cs
@@ -1,10 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarResetter : MonoBehaviour {
 
     public bool resettingCar = false;
+
+    public GameObject carPlacer;
+
+    public float placementSpeed = 1;
+
+    [Header("Ranges")]
+    public float rangeXMax = 0f;
+    public float rangeXMin = 0f;
+    public float rangeYMax = 0f;
+    public float rangeYMin = 0f;
+    
+
+
+    void FixedUpdate()
+    {
+        if (resettingCar)
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                carPlacer.transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * placementSpeed);
+            }
+            if (Input.GetAxis("Tilt") != 0)
+            {
+                carPlacer.transform.Translate(Vector3.up * Input.GetAxis("Tilt") * placementSpeed);
+            }
+            Vector2 placerPosition = carPlacer.transform.localPosition;
+            float placerX = (Mathf.Clamp(placerPosition.x, rangeXMin, rangeXMax));
+            float placerY = (Mathf.Clamp(placerPosition.y, rangeYMin, rangeYMax));
+            carPlacer.transform.localPosition = new Vector2(placerX, placerY);
+
+            //this.transform.LookAt(ball.transform);
+
+        }
+    }
+            public void ResetterStartReset()
+    {
+        resettingCar = true;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // FIRST ATTEMPT, DIDN"T WORK
+    /*public bool resettingCar = false;
     public GameObject ball;
     MeshRenderer meshRenderer;
 
@@ -35,5 +91,5 @@ public class CarResetter : MonoBehaviour {
             //this.transform.LookAt(ball.transform);
         }
 
-	}
-}
+	}*/
+
