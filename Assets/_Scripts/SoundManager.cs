0cd1791d2ebab81d7b8b82df8530b44d0e3f00b0
Miles Robson <miles_robson@yahoo.com>
2018-04-24 22:33:31 -0400

    Added basic sound manager,adjusted sounds

---
+++ Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip rev;
+	public AudioClip boost;
+
+	public AudioClip boostTailLoop;
+
+	public AudioSource mySource;
+
+	// Use this for initialization
+	void Start () {
+		mySource = this.GetComponent<AudioSource>();
+		PlayRevLoop();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		
+	}
+
+	void PlayRevLoop()
+	{
+		mySource.clip = rev;
+		mySource.loop = true;
+		mySource.Play();
+
+	}
+
+	void PlayBoostOneOff()
+	{
+		mySource.clip = boost;
+		mySource.loop = false;
+		mySource.Play();
+
+	}
+	void PlayBoostTailLoop()
+	{
+		mySource.clip = boostTailLoop;
+		mySource.loop = true;
+		mySource.Play();
+
+	}
+}
