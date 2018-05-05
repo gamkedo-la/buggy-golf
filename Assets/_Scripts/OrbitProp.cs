27108fca7f40d68a4bf9874395a4953160f538b4
McFunkypants <breakdance@mcfunkypants.com>
2018-05-04 21:19:16 -0700

    tiny background spaceships w glowing trails and orbital movement

---
+++ Assets/_Scripts/OrbitProp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitProp : MonoBehaviour {
+
+	public Vector3 center;
+	public Vector3 axis = Vector3.up;
+	public Vector3 desiredPosition;
+	public float radius = 2.0f;
+	public float radiusSpeed = 0.5f;
+	public float rotationSpeed = 80.0f;
+
+	void Start () {
+		transform.position = (transform.position - center).normalized * radius + center;
+	}
+
+	void Update () {
+		transform.RotateAround (center, axis, rotationSpeed * Time.deltaTime);
+		desiredPosition = (transform.position - center).normalized * radius + center;
+		transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+	}
+}
